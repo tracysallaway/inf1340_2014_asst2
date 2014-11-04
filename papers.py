0461959ca7b885be27499851145030d991a2e7bc
Jodie Church <jodie.church@gmail.com>
2014-11-03 20:34:36 -0500

    First commit - some IF statements
    Tried doing some if statements - not sure if this is the direction that
    we’re supposed to go, or if I’m totally off base.

--- papers.py
+++ papers.py
@@ -26,7 +26,69 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-    return ["Reject"]
+    with open(input_file, "r") as file_reader:
+        file_contents = file_reader.read()
+        example_entries = json.loads(file_contents)
+
+    with open(countries_file, "r") as file_reader:
+        file_contents = file_reader.read()
+        countries = json.loads(file_contents)
+
+    with open(watchlist_file, "r") as file_reader:
+        file_contents = file_reader.read()
+        watchlist = json.loads(file_contents)
+    temp=[]
+    for i in range(len(example_entries)):
+        #validity=True
+        #validity=validity and not example_entries[i]["first_name"]==None
+        #validity = validity and not example_entries[i]["last_name"] == None
+        #validity = validity and valid_date_format(example_entries[i]["birth_date"])
+        #validity = validity and valid_passport_format(example_entries[i]["passport"])
+        #validity = validity and not example_entries[i]["home"] == None
+        choice='Reject'
+        if not example_entries[i]["first_name"] == None:
+            if not example_entries[i]["last_name"] == None:
+                if valid_date_format(example_entries[i]["birth_date"]):
+                    if valid_passport_format(example_entries[i]["passport"]):
+                        if not example_entries[i]["home"] == None :
+                            if not example_entries[i]["home"]["city"] == None:
+                                if not example_entries[i]["home"]["region"] == None:
+                                    if not example_entries[i]["home"]["country"] == None:
+                                        if not example_entries[i]["from"] == None :
+                                            if not example_entries[i]["from"]["city"] == None:
+                                                if not example_entries[i]["from"]["region"] == None:
+                                                    if not example_entries[i]["from"]["country"] == None:
+                                                        if not example_entries[i]["entry_reason"] == None:
+
+                                                            if example_entries[i]["home"]["country"] == "KAN":
+                                                                if example_entries[i]["entry_reason"] == "returning":
+                                                                    choice = "Accept"
+                                                            else:
+                                                                if example_entries[i]["entry_reason"] == "visit":
+                                                                    if countries[example_entries[i]["home"]["country"]]["visitor_visa_required"] == 1:
+                                                                        if "visa" in example_entries[i].keys():
+                                                                            if True: # validate date example_entries[i]["visa"]["date"]
+                                                                                choice = "Accept"
+                                                                elif example_entries[i]["entry_reason"] == "transit":
+                                                                    if countries[example_entries[i]["home"]["country"]]["transit_visa_required"] == 1:
+                                                                        if "visa" in example_entries[i].keys():
+                                                                            if True: # validate date example_entries[i]["visa"]["date"]
+                                                                                choice = "Accept"
+                                                                if choice == "Accept":
+                                                                    for j in range(len(watchlist)):
+                                                                        not_present=True
+                                                                        if True: # not (not_present and ((first_name1==first_name2 and last_name1==last_name4) or passp1==passp2)):
+                                                                            choice = "Secondary"
+
+                                                                if "via" in example_entries[i].keys():
+                                                                    if not countries[example_entries[i]["via"]["country"]]["medical_advisory"] == None:
+                                                                        choice = "Quarantine"
+                                                                if not countries[example_entries[i]["home"]["country"]]["medical_advisory"] == None:
+                                                                    choice = "Quarantine"
+        temp.append(choice)
+    return(temp)
+
+    #return ["Reject"]
 
 
 def valid_passport_format(passport_number):
@@ -54,3 +116,9 @@ def valid_date_format(date_string):
         return True
     except ValueError:
         return False
+
+
+print(decide("example_entries.json", "watchlist.json", "countries.json"))
+
+
+
